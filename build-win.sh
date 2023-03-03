bb2102af74384c704c7699bd07cc276172cc38a9
David Choi <david.choi@philabs.xyz>
2023-03-02 19:43:55 -0500

    try win with mingw compiler and new build:go:win

--- build-win.sh
+++ build-win.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-go build -trimpath -buildmode=c-shared -ldflags '-w -s' -o keyring.so ./src/go
+go build -trimpath -buildmode=c-shared -ldflags '-w -s' -o keyring.dll ./src/go
+gendef keyring.dll
+#dlltool --input-def keyring.def --output-lib keyring.lib
+#cl.exe main.c foo.lib
