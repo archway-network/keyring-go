6179083e7b004ffb2d0642fe3b9f7e2428869f14
eliasmpw <eliasmpw@hotmail.com>
2023-08-17 13:20:19 +0200

    test: add cgo config

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-go build -trimpath -buildmode=c-archive -ldflags '-w -s -extldflags "-lresolv"' -o keyring.a -a ./src/go
+CGO_ENABLED=1 go build -trimpath -buildmode=c-archive -ldflags '-w -s -extldflags "-lresolv"' -o keyring.a -a ./src/go
