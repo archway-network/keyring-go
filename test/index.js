94091537cfbf42d675412f7c01716b07146f9919
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-08-29 11:57:18 +0200

    ci: skip OS keyring test in CI

--- test/index.js
+++ test/index.js
@@ -6,14 +6,12 @@ const context = {
   tmpDir: fs.mkdtempSync(path.join(os.tmpdir(), "archway-keyring-test-")),
 };
 
-[
-  !process.env.SKIP_FILE && "file",
-  !process.env.SKIP_OS && "os",
-  !process.env.SKIP_UNENCRYPTED && "unencrypted",
-].filter(item => item).forEach((test) => {
-  console.log(`\n=> Testing ${test}`);
-  require(`./${test}`).run(context);
-});
+["file", process.env.CI != "true" && "os", "unencrypted"]
+  .filter((item) => item)
+  .forEach((test) => {
+    console.log(`\n=> Testing ${test}`);
+    require(`./${test}`).run(context);
+  });
 
 try {
   fs.rmSync(context.tmpDir, { recursive: true });
